1641ce76e872bb0cb4193f09dd511659585f83ae
sano820 <sano820@naver.com>
2026-01-07 15:18:12 +0900

    feat: 유틸리티 함수 구현 - validate_numbers : 리스트의 모든 요소가 숫자인지 검증 - round_result : 숫자를 지정한 소수점 자리수를 반올림 - format_output : 포매팅된 문자열로 반환

--- src/utils.py
+++ src/utils.py
@@ -27,7 +27,7 @@ def validate_numbers(data: List[Any]) -> bool:
     """
     # TODO: 구현하세요
     # 힌트: isinstance(x, (int, float)) 사용
-    pass
+    return all(isinstance(x, (int, float)) for x in data)
 
 
 def round_result(value: float, decimals: int = 2) -> float:
@@ -48,7 +48,7 @@ def round_result(value: float, decimals: int = 2) -> float:
     """
     # TODO: 구현하세요
     # 힌트: round() 내장 함수 사용
-    pass
+    return round(value, decimals)
 
 
 def format_output(name: str, value: float, decimals: int = 2) -> str:
@@ -70,4 +70,5 @@ def format_output(name: str, value: float, decimals: int = 2) -> str:
     """
     # TODO: 구현하세요
     # 힌트: f-string 또는 format() 사용
-    pass
+    formatted_value = f"{value:.{decimals}f}"
+    return f"{name}: {formatted_value}"
